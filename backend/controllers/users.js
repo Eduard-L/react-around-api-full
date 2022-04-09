be06b7a344c0c3b6b18426409720b4b9955f6c19
Eduard-L <lwqtbdwrd@gmail.com>
2022-04-09 11:21:25 +0300

    fix:permision for database

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -3,7 +3,7 @@ const { User, ADMIN_ID } = require('../utils/constants')
 const { NotFoundError, BadRequestError, ForbiddentError } = require('../utils/errorHandler')
 
 
-console.log(typeof (ADMIN_ID))
+console.log(ADMIN_ID === "624e8b87bf2e7d35acf0d867")
 const getUsersData = async (req, res, next) => {
   const { _id } = req.user
   try {
